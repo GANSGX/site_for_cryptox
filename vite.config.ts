70c4895c1f1ba29b382edea0e8e4f5122b9e7aba
GANSGX <takeshigans@gmail.com>
2025-10-25 11:31:14 +0700

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/site_for_messanger/', // ← ИМЯ ТВОЕГО РЕПОЗИТОРИЯ
+  base: '/site_for_cryptox/', // ← БЫЛО site_for_messanger
 })
